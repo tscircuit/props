c9a446f4634968d264c8f17d5e732e98265fc86f
Severin Ibarluzea <seveibar@gmail.com>
2024-07-10 13:59:07 -0700

    add biome, extend cadModel with positionOffset and rotationOffset (#5)
    * add support for cadModel prop
    
    * add biome, extend cadModel with positionOffset and rotationOffset

--- lib/index.ts
+++ lib/index.ts
@@ -64,16 +64,28 @@ export const supplierProps = z.object({
 })
 export type SupplierProps = z.input<typeof supplierProps>
 
-export const cadModelStl = z.object({
+const point3 = z.object({
+  x: z.union([z.number(), z.string()]),
+  y: z.union([z.number(), z.string()]),
+  z: z.union([z.number(), z.string()]),
+})
+
+export const cadModelBase = z.object({
+  rotationOffset: z.number().or(point3).optional(),
+  positionOffset: point3.optional(),
+  size: point3.optional(),
+})
+
+export const cadModelStl = cadModelBase.extend({
   stlUrl: z.string(),
 })
 
-export const cadModelObj = z.object({
+export const cadModelObj = cadModelBase.extend({
   objUrl: z.string(),
   mtlUrl: z.string().optional(),
 })
 
-export const cadModelJscad = z.object({
+export const cadModelJscad = cadModelBase.extend({
   jscad: z.any(),
 })
 

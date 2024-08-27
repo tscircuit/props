ca5eeb7fdb85216a7c9ee2b8265fe9655a6d5adc
seveibar <seveibar@gmail.com>
2024-08-27 15:08:21 -0700

    add testing and template for new structure

---
+++ tests/chip.test.ts
@@ -0,0 +1,31 @@
+import { expect, test } from "bun:test"
+import { chipProps, type ChipProps } from "lib/index"
+import type { z } from "zod"
+import { expectTypeOf } from "expect-type"
+
+test("should parse chip props", () => {
+  const rawProps: ChipProps = {
+    name: "chip",
+    manufacturerPartNumber: "1234",
+    pinLabels: {
+      1: "1",
+      2: "2",
+      3: "3",
+      4: "4",
+    },
+    schPortArrangement: {
+      leftSize: 1,
+      topSize: 2,
+      rightSize: 3,
+      bottomSize: 4,
+    },
+    schPinSpacing: "0.2mm",
+    schWidth: 2,
+  }
+
+  expectTypeOf(rawProps).toMatchTypeOf<z.input<typeof chipProps>>()
+
+  const parsedProps = chipProps.parse(rawProps)
+
+  expect(parsedProps.schPinSpacing).toBe(0.2)
+})

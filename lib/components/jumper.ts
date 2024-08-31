472e966c02d4d3b82682615c527d82da5f401ef5
Severin Ibarluzea <seveibar@gmail.com>
2024-08-30 18:04:46 -0700

    add schematic direction to jumper (#36)
    * add schematic direction to jumper
    
    * better jumper types
    
    * fix type issue

--- lib/components/jumper.ts
+++ lib/components/jumper.ts
@@ -9,6 +9,10 @@ import {
 } from "lib/common/schematicPinStyle"
 import { distance } from "@tscircuit/soup"
 import { expectTypesMatch } from "lib/typecheck"
+import {
+  schematicPortArrangement,
+  type SchematicPortArrangement,
+} from "lib/common/schematicPinDefinitions"
 
 export interface JumperProps extends CommonComponentProps {
   manufacturerPartNumber?: string
@@ -17,6 +21,8 @@ export interface JumperProps extends CommonComponentProps {
   schPinSpacing?: number | string
   schWidth?: number | string
   schHeight?: number | string
+  schDirection?: "left" | "right"
+  schPortArrangement?: SchematicPortArrangement
 }
 
 export const jumperProps = commonComponentProps.extend({
@@ -26,6 +32,8 @@ export const jumperProps = commonComponentProps.extend({
   schPinSpacing: distance.optional(),
   schWidth: distance.optional(),
   schHeight: distance.optional(),
+  schDirection: z.enum(["left", "right"]).optional(),
+  schPortArrangement: schematicPortArrangement.optional(),
 })
 
 type InferredJumperProps = z.input<typeof jumperProps>

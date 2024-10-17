ee62f020742d4a1bcdd9d3f39a6e77060b31c1da
seveibar <seveibar@gmail.com>
2024-10-17 09:36:46 -0700

    Battery types, batteryProps, set default unit to mAh

---
+++ lib/components/battery.ts
@@ -0,0 +1,35 @@
+import { z } from "zod"
+import {
+  commonComponentProps,
+  lrPins,
+  lrPolarPins,
+  type CommonComponentProps,
+} from "lib/common/layout"
+import { expectTypesMatch } from "lib/typecheck"
+
+const capacity = z
+  .number()
+  .or(z.string().endsWith("mAh"))
+  .transform((v) => {
+    if (typeof v === "string") {
+      const valString = v.replace("mAh", "")
+      const num = Number.parseFloat(valString)
+      if (Number.isNaN(num)) {
+        throw new Error("Invalid capacity")
+      }
+      return num
+    }
+    return v
+  })
+  .describe("Battery capacity in mAh")
+
+export interface BatteryProps extends CommonComponentProps {
+  capacity?: number | string
+}
+
+export const batteryProps = commonComponentProps.extend({
+  capacity: capacity.optional(),
+})
+export const batteryPins = lrPolarPins
+
+expectTypesMatch<BatteryProps, z.input<typeof batteryProps>>(true)

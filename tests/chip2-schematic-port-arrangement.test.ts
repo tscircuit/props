4759771d3aa527924ceb7b12561f484e7ae7ad84
Rishabh Gupta <belikerishabh@gmail.com>
2024-11-07 20:05:27 +0530

    feat: Add pins as string

--- tests/chip2-schematic-port-arrangement.test.ts
+++ tests/chip2-schematic-port-arrangement.test.ts
@@ -3,7 +3,7 @@ import { chipProps, type ChipProps } from "lib/components/chip"
 import type { z } from "zod"
 import { expectTypeOf } from "expect-type"
 
-test("should parse chip props", () => {
+test("should parse chip props (number)", () => {
   const rawProps: ChipProps = {
     name: "chip",
     manufacturerPartNumber: "1234",
@@ -37,5 +37,36 @@ test("should parse chip props", () => {
 
   expect((parsedProps.schPortArrangement as any)?.leftSide.pins).toEqual([
     29, 7, 8, 20, 19, 22,
+  ])
+})
+
+test("should parse chip props (string)", () => {
+  const rawProps: ChipProps = {
+    name: "chip",
+    manufacturerPartNumber: "1234",
+    pinLabels: {
+      1: "1",
+      2: "2",
+      3: "3",
+      4: "4",
+    },
+    schPortArrangement: {
+      leftSide: {
+        pins: ["29", "7", "8", "20", "19", "22"],
+        direction: "top-to-bottom",
+      },
+      topSide: {
+        direction: "left-to-right",
+        pins: ["A4", "B18"],
+      },
+    },
+    schPinSpacing: "0.2mm",
+    schWidth: 2,
+  }
+
+  const parsedProps = chipProps.parse(rawProps)
+
+  expect((parsedProps.schPortArrangement as any)?.topSide.pins).toEqual([
+    "A4", "B18",
   ])
 })

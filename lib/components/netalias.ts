d28cc73b6bd9e90e807992786ccc37f7fb7d9b2e
seveibar <seveibar@gmail.com>
2024-11-11 11:32:33 -0800

    fix schRotation for netalias

--- lib/components/netalias.ts
+++ lib/components/netalias.ts
@@ -1,11 +1,13 @@
 import { z } from "zod"
 import { expectTypesMatch } from "lib/typecheck"
 import { distance } from "lib/common/distance"
+import { rotation } from "circuit-json"
 
 export interface NetAliasProps {
   net?: string
   schX?: number | string
   schY?: number | string
+  schRotation?: number | string
   anchorSide?: "left" | "up" | "right" | "down"
 }
 
@@ -13,6 +15,7 @@ export const netAliasProps = z.object({
   net: z.string().optional(),
   schX: distance.optional(),
   schY: distance.optional(),
+  schRotation: rotation.optional(),
   anchorSide: z.enum(["left", "up", "right", "down"]).optional(),
 })
 

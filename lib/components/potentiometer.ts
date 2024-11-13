aa30cb4dc595046ff22979eb395a48c54539e52c
Rishabh Gupta <belikerishabh@gmail.com>
2024-11-14 02:47:24 +0530

    feat: add potentiometer

---
+++ lib/components/potentiometer.ts
@@ -0,0 +1,22 @@
+import { resistance } from "circuit-json"
+import {
+    type CommonComponentProps,
+    commonComponentProps,
+    lrPins,
+} from "lib/common/layout"
+import { expectTypesMatch } from "lib/typecheck"
+import { z } from "zod"
+
+export interface PotentiometerProps extends CommonComponentProps {
+  resistance: number | string
+  wiper?: number
+}
+
+export const potentiometerProps = commonComponentProps.extend({
+  resistance,
+  wiper: z.number().min(0).max(1).optional().default(0.5),
+})
+export const potentiometerPins = lrPins
+
+type InferredPotentiometerProps = z.input<typeof potentiometerProps>
+expectTypesMatch<PotentiometerProps, InferredPotentiometerProps>(true)

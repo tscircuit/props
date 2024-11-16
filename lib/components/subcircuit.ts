c5aa70b605fb0f6ff0735a210f9b1cf07f5256c4
seveibar <seveibar@gmail.com>
2024-11-16 15:25:20 -0800

    add subcircuit file

---
+++ lib/components/subcircuit.ts
@@ -0,0 +1,11 @@
+import type { SubcircuitGroupProps } from "./group"
+import { subcircuitGroupProps } from "./group"
+import type { z } from "zod"
+import { expectTypesMatch } from "lib/typecheck"
+
+export type SubcircuitProps = SubcircuitGroupProps
+
+export const subcircuitProps = subcircuitGroupProps
+
+type InferredSubcircuitProps = z.input<typeof subcircuitProps>
+expectTypesMatch<SubcircuitProps, InferredSubcircuitProps>(true)

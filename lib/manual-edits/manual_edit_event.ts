89a160bc337b606a13cbb416eed71353a48642b5
seveibar <seveibar@gmail.com>
2024-12-16 23:31:08 -0800

    fix exports to include manual edit events

--- lib/manual-edits/manual_edit_event.ts
+++ lib/manual-edits/manual_edit_event.ts
@@ -1,19 +1,27 @@
 import { z } from "zod"
 import {
-  edit_component_location_event,
+  edit_pcb_component_location_event,
   type EditPcbComponentLocationEvent,
 } from "./manual-edit-events/edit_pcb_component_location_event"
+import {
+  edit_schematic_component_location_event,
+  type EditSchematicComponentLocationEvent,
+} from "./manual-edit-events/edit_schematic_component_location_event"
 import {
   edit_trace_hint_event,
   type EditTraceHintEvent,
 } from "./manual-edit-events/edit_trace_hint_event"
 import { expectTypesMatch } from "lib/typecheck"
 
-export type ManualEditEvent = EditPcbComponentLocationEvent | EditTraceHintEvent
+export type ManualEditEvent =
+  | EditPcbComponentLocationEvent
+  | EditTraceHintEvent
+  | EditSchematicComponentLocationEvent
 
 export const manual_edit_event = z.union([
-  edit_component_location_event,
+  edit_pcb_component_location_event,
   edit_trace_hint_event,
+  edit_schematic_component_location_event,
 ])
 
 export type ManualEditEventInput = z.input<typeof manual_edit_event>

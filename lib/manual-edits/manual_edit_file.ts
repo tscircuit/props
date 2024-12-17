a1d4ae4d2dce59446d96ee51d666ce68bf4cefce
seveibar <seveibar@gmail.com>
2024-12-16 21:48:43 -0800

    minor type fix

--- lib/manual-edits/manual_edit_file.ts
+++ lib/manual-edits/manual_edit_file.ts
@@ -1,6 +1,7 @@
 import { z } from "zod"
 import { manual_pcb_placement } from "./manual_pcb_position"
 import { manual_edit_event } from "./manual_edit_event"
+import { manual_trace_hint } from "./manual_trace_hint"
 
 export const manual_edit_file = z.object({
   pcb_placements: z.array(manual_pcb_placement).optional(),

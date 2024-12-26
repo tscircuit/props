439fc6cc6cbf54f5ebe8a4d66dcea10a89e261a6
Anshgrover23 <ag5989670@gmail.com>
2024-12-26 23:36:26 +0530

    fix: test

--- tests/transistor.test.ts
+++ tests/transistor.test.ts
@@ -5,59 +5,30 @@ import {
 } from "../lib/components/transistor"
 import { expectTypeOf } from "expect-type"
 
-test("should parse transistor props for NPN transistor", () => {
+test("should parse transistor props for npn transistor", () => {
   const rawProps: TransistorProps = {
     name: "transistor",
-    transistorType: "NPN",
-    pinLabels: {
-      emitter: "E",
-      base: "B",
-      collector: "C",
-    },
-    collectorEmitterVoltage: "5V",
-    baseEmitterVoltage: "0.7V",
-    collectorCurrent: "10mA",
+    transistorType: "npn",
   }
 
   const parsedProps = transistorProps.parse(rawProps)
-  expect(parsedProps.transistorType).toBe("NPN")
-  expect(parsedProps.collectorEmitterVoltage).toBe(5)
-  expect(parsedProps.collectorCurrent).toBe(0.01)
+  expect(parsedProps.transistorType).toBe("npn")
 })
 
-test("should parse transistor props for PNP transistor", () => {
+test("should parse transistor props for pnp transistor", () => {
   const rawProps: TransistorProps = {
     name: "transistor",
-    transistorType: "PNP",
-    pinLabels: {
-      emitter: "E",
-      base: "B",
-      collector: "C",
-    },
-    collectorEmitterVoltage: 3.3,
-    baseEmitterVoltage: 0.6,
-    collectorCurrent: 20,
+    transistorType: "pnp",
   }
 
   const parsedProps = transistorProps.parse(rawProps)
-  expect(parsedProps.transistorType).toBe("PNP")
-  expect(parsedProps.collectorEmitterVoltage).toBe(3.3)
-  expect(parsedProps.baseEmitterVoltage).toBe(0.6)
-  expect(parsedProps.collectorCurrent).toBe(20)
+  expect(parsedProps.transistorType).toBe("pnp")
 })
 
 test("should fail to parse transistor props with invalid transistor type", () => {
   const rawProps = {
     name: "transistor",
     transistorType: "INVALID",
-    pinLabels: {
-      emitter: "E",
-      base: "B",
-      collector: "C",
-    },
-    collectorEmitterVoltage: "5V",
-    baseEmitterVoltage: "0.7V",
-    collectorCurrent: "10mA",
   }
 
   expect(() => transistorProps.parse(rawProps)).toThrow()
@@ -66,15 +37,7 @@ test("should fail to parse transistor props with invalid transistor type", () =>
 test("should enforce correct types for transistor props", () => {
   const rawProps: TransistorProps = {
     name: "transistor",
-    transistorType: "NPN",
-    pinLabels: {
-      emitter: "E",
-      base: "B",
-      collector: "C",
-    },
-    collectorEmitterVoltage: "5V",
-    baseEmitterVoltage: "0.7V",
-    collectorCurrent: "10mA",
+    transistorType: "npn",
   }
 
   expectTypeOf(rawProps).toMatchTypeOf<TransistorProps>()

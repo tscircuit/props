bf2b261653d2acc1908fe82ac22756187073c079
tscircuitbot <tscircuitbot@users.noreply.github.com>
2024-12-29 03:52:20 +0000

    formatbot: Automatically format code

--- tests/mosfet.test.ts
+++ tests/mosfet.test.ts
@@ -6,7 +6,7 @@ test("should parse mosfet props for n-channel", () => {
   const rawProps: MosfetProps = {
     name: "mosfet",
     mosfetType: "n_channel_moset",
-    schRotation: 0
+    schRotation: 0,
   }
   const parsedProps = mosfetProps.parse(rawProps)
   expect(parsedProps.mosfetType).toBe("n_channel_moset")
@@ -16,7 +16,7 @@ test("should parse mosfet props for p-channel", () => {
   const rawProps: MosfetProps = {
     name: "mosfet",
     mosfetType: "p_channel_moset",
-    schRotation: 90
+    schRotation: 90,
   }
   const parsedProps = mosfetProps.parse(rawProps)
   expect(parsedProps.mosfetType).toBe("p_channel_moset")
@@ -34,7 +34,7 @@ test("should enforce correct types for mosfet props", () => {
   const rawProps: MosfetProps = {
     name: "mosfet",
     mosfetType: "n_channel_moset",
-    schRotation: 180
+    schRotation: 180,
   }
   expectTypeOf(rawProps).toMatchTypeOf<MosfetProps>()
 })

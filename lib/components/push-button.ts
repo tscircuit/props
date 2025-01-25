3151546da42d8912b38161257779b15b3053bf3b
seveibar <seveibar@gmail.com>
2025-01-24 17:15:40 -0800

    add default configuration for internally connected push button pins

--- lib/components/push-button.ts
+++ lib/components/push-button.ts
@@ -1,5 +1,20 @@
-import { commonComponentProps } from "lib/common/layout"
+import {
+  commonComponentProps,
+  type CommonComponentProps,
+} from "lib/common/layout"
+import { expectTypesMatch } from "lib/typecheck"
 import { z } from "zod"
 
-export const pushButtonProps = commonComponentProps.extend({})
-export type PushButtonProps = z.input<typeof pushButtonProps>
+export interface PushButtonProps extends CommonComponentProps {
+  internallyConnectedPorts?: string[][]
+}
+
+export const pushButtonProps = commonComponentProps.extend({
+  internallyConnectedPorts: z.array(z.array(z.string())).default([
+    ["pin1", "pin4"],
+    ["pin2", "pin3"],
+  ]),
+})
+type InferredPushButtonProps = z.input<typeof pushButtonProps>
+
+expectTypesMatch<PushButtonProps, InferredPushButtonProps>(true)

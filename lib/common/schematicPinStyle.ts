16f6c1e6af9b81c6fec41f3c6518e1f7d1139dc8
seveibar <seveibar@gmail.com>
2025-01-25 10:44:48 -0800

    fix convention of schPinStyle

--- lib/common/schematicPinStyle.ts
+++ lib/common/schematicPinStyle.ts
@@ -5,15 +5,29 @@ import { z } from "zod"
 export type SchematicPinStyle = Record<
   string,
   {
+    marginTop?: number | string
+    marginRight?: number | string
+    marginBottom?: number | string
+    marginLeft?: number | string
+
+    /** @deprecated use marginLeft */
     leftMargin?: number | string
+    /** @deprecated use marginRight */
     rightMargin?: number | string
+    /** @deprecated use marginTop */
     topMargin?: number | string
+    /** @deprecated use marginBottom */
     bottomMargin?: number | string
   }
 >
 
 export const schematicPinStyle = z.record(
   z.object({
+    marginLeft: distance.optional(),
+    marginRight: distance.optional(),
+    marginTop: distance.optional(),
+    marginBottom: distance.optional(),
+
     leftMargin: distance.optional(),
     rightMargin: distance.optional(),
     topMargin: distance.optional(),

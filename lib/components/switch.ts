ad72058ec38c8798aeeb03374b71b37264302fe1
techmannih <manishchaudharyttt@gmail.com>
2025-02-08 09:16:09 +0530

    improve switch props

--- lib/components/switch.ts
+++ lib/components/switch.ts
@@ -1,9 +1,22 @@
-import { commonComponentProps } from "lib/common/layout"
+import {
+  type CommonComponentProps,
+  commonComponentProps,
+} from "lib/common/layout"
+import { expectTypesMatch } from "lib/typecheck"
+
 import { z } from "zod"
+
+export interface SwitchProps extends CommonComponentProps {
+  ftype: "switch"
+  type: "spst" | "spdt" | "dpst" | "dpdt"
+  isNormallyClosed?: boolean
+}
 
 export const switchProps = commonComponentProps.extend({
   ftype: z.literal("switch"),
-  switchType: z.enum(["spst"]).default("spst"),
-  isNormallyClosed: z.boolean().default(false),
+  type: z.enum(["spst", "spdt", "dpst", "dpdt"]),
+  isNormallyClosed: z.boolean().optional().default(false),
 })
-export type SwitchProps = z.input<typeof switchProps>
+
+export type InferredSwitchProps = z.input<typeof switchProps>
+expectTypesMatch<SwitchProps, InferredSwitchProps>(true)

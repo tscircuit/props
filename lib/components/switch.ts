108b372de9fc4b1469c71ddd3d70d19ff0d30a05
techmannih <manishchaudharyttt@gmail.com>
2025-02-08 14:55:41 +0530

    add types as boolean

--- lib/components/switch.ts
+++ lib/components/switch.ts
@@ -10,13 +10,36 @@ export interface SwitchProps extends CommonComponentProps {
   ftype: "switch"
   type: "spst" | "spdt" | "dpst" | "dpdt"
   isNormallyClosed?: boolean
+  isSPDT?: boolean
+  isSPST?: boolean
+  isDPST?: boolean
+  isDPDT?: boolean
 }
 
 export const switchProps = commonComponentProps.extend({
   ftype: z.literal("switch"),
   type: z.enum(["spst", "spdt", "dpst", "dpdt"]),
   isNormallyClosed: z.boolean().optional().default(false),
+}).transform((val) => {
+  // Add boolean properties based on 'type'
+  const updatedVal: SwitchProps = { ...val }
+  switch (updatedVal.type) {
+    case "spdt":
+      updatedVal.isSPDT = true
+      break
+    case "spst":
+      updatedVal.isSPST = true
+      break
+    case "dpst":
+      updatedVal.isDPST = true
+      break
+    case "dpdt":
+      updatedVal.isDPDT = true
+      break
+  }
+
+  return updatedVal
 })
 
-export type InferredSwitchProps = z.input<typeof switchProps>
+export type InferredSwitchProps = z.infer<typeof switchProps>
 expectTypesMatch<SwitchProps, InferredSwitchProps>(true)

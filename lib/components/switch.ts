f2bc87f9ca5efbaeca79619bc076209b67b0158a
techmannih <manishchaudharyttt@gmail.com>
2025-02-08 14:55:58 +0530

    format code

--- lib/components/switch.ts
+++ lib/components/switch.ts
@@ -16,30 +16,32 @@ export interface SwitchProps extends CommonComponentProps {
   isDPDT?: boolean
 }
 
-export const switchProps = commonComponentProps.extend({
-  ftype: z.literal("switch"),
-  type: z.enum(["spst", "spdt", "dpst", "dpdt"]),
-  isNormallyClosed: z.boolean().optional().default(false),
-}).transform((val) => {
-  // Add boolean properties based on 'type'
-  const updatedVal: SwitchProps = { ...val }
-  switch (updatedVal.type) {
-    case "spdt":
-      updatedVal.isSPDT = true
-      break
-    case "spst":
-      updatedVal.isSPST = true
-      break
-    case "dpst":
-      updatedVal.isDPST = true
-      break
-    case "dpdt":
-      updatedVal.isDPDT = true
-      break
-  }
+export const switchProps = commonComponentProps
+  .extend({
+    ftype: z.literal("switch"),
+    type: z.enum(["spst", "spdt", "dpst", "dpdt"]),
+    isNormallyClosed: z.boolean().optional().default(false),
+  })
+  .transform((val) => {
+    // Add boolean properties based on 'type'
+    const updatedVal: SwitchProps = { ...val }
+    switch (updatedVal.type) {
+      case "spdt":
+        updatedVal.isSPDT = true
+        break
+      case "spst":
+        updatedVal.isSPST = true
+        break
+      case "dpst":
+        updatedVal.isDPST = true
+        break
+      case "dpdt":
+        updatedVal.isDPDT = true
+        break
+    }
 
-  return updatedVal
-})
+    return updatedVal
+  })
 
 export type InferredSwitchProps = z.infer<typeof switchProps>
 expectTypesMatch<SwitchProps, InferredSwitchProps>(true)

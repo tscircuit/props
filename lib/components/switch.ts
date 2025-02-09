25db67df3582320331b777ac79d64313bb5071b6
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-02-09 23:23:01 +0530

    improve switch props (#164)
    * improve switch props
    
    * add types as boolean
    
    * format code
    
    * change var name and make type optional
    
    * remove banned name
    
    * updatedprops

--- lib/components/switch.ts
+++ lib/components/switch.ts
@@ -1,9 +1,47 @@
-import { commonComponentProps } from "lib/common/layout"
+import {
+  type CommonComponentProps,
+  commonComponentProps,
+} from "lib/common/layout"
+import { expectTypesMatch } from "lib/typecheck"
+
 import { z } from "zod"
 
-export const switchProps = commonComponentProps.extend({
-  ftype: z.literal("switch"),
-  switchType: z.enum(["spst"]).default("spst"),
-  isNormallyClosed: z.boolean().default(false),
-})
-export type SwitchProps = z.input<typeof switchProps>
+export interface SwitchProps extends CommonComponentProps {
+  ftype: "switch"
+  type?: "spst" | "spdt" | "dpst" | "dpdt"
+  isNormallyClosed?: boolean
+  spdt?: boolean
+  spst?: boolean
+  dpst?: boolean
+  dpdt?: boolean
+}
+
+export const switchProps = commonComponentProps
+  .extend({
+    ftype: z.literal("switch"),
+    type: z.enum(["spst", "spdt", "dpst", "dpdt"]).optional(),
+    isNormallyClosed: z.boolean().optional().default(false),
+  })
+  .transform((props) => {
+    // Add boolean properties based on 'type'
+    const updatedProps: SwitchProps = { ...props }
+    switch (updatedProps.type) {
+      case "spdt":
+        updatedProps.spdt = true
+        break
+      case "spst":
+        updatedProps.spst = true
+        break
+      case "dpst":
+        updatedProps.dpst = true
+        break
+      case "dpdt":
+        updatedProps.dpdt = true
+        break
+    }
+
+    return updatedProps
+  })
+
+export type InferredSwitchProps = z.infer<typeof switchProps>
+expectTypesMatch<SwitchProps, InferredSwitchProps>(true)

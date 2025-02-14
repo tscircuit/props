33a90a75c5065b93ccf2289616937387eafcaba6
seveibar <seveibar@gmail.com>
2025-02-14 14:39:34 -0800

    autorouter url

--- lib/platformConfig.ts
+++ lib/platformConfig.ts
@@ -11,11 +11,17 @@ export interface PlatformConfig {
   partsEngine?: PartsEngine
 
   autorouter?: AutorouterProp
+
+  registryApiUrl?: string
+
+  cloudAutorouterUrl?: string
 }
 
 export const platformConfig = z.object({
   partsEngine: partsEngine.optional(),
   autorouter: autorouterProp.optional(),
+  registryApiUrl: z.string().optional(),
+  cloudAutorouterUrl: z.string().optional(),
 })
 
 expectTypesMatch<PlatformConfig, z.infer<typeof platformConfig>>(true)

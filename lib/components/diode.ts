cc0b46a88e8708505a5ee69e4eaba04cb49182d1
Anshgrover23 <ag5989670@gmail.com>
2025-03-28 01:00:43 +0530

    remove extra comments

--- lib/components/diode.ts
+++ lib/components/diode.ts
@@ -1,8 +1,7 @@
 import { commonComponentProps, lrPolarPins } from "lib/common/layout"
-import { z } from "zod" // Import z as a value
-import type { Connections } from "./chip" // Import Connections as a type
+import { z } from "zod"
+import type { Connections } from "./chip"
 
-// Define connectionsProp using Connections
 const connectionTarget = z
   .string()
   .or(z.array(z.string()).readonly())
@@ -13,7 +12,7 @@ const connectionsProp = z
   .pipe(z.record(z.string(), connectionTarget))
 
 export const diodeProps = commonComponentProps.extend({
-  connections: connectionsProp.optional(), // Add the connections property
+  connections: connectionsProp.optional(),
 })
 
 export const diodePins = lrPolarPins

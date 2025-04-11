88fafef0ec188d4271e8f65836ebebea1774e5f8
seveibar <seveibar@gmail.com>
2025-04-11 10:39:24 -0700

    more docstring improvements

--- lib/utility-types/connections-and-selectors.ts
+++ lib/utility-types/connections-and-selectors.ts
@@ -65,6 +65,24 @@ export type ConnectionTarget = string
  *   GND: ".U1 > .GND",
  *   VCC: ".U1 > .VCC",
  * }
+ *
+ * Connections are used as both inputs and outputs. For example, you might
+ * receive connections when using `sel` to select a chip.
+ *
+ * const u1Connections = sel.U1(MyChip)
+ *
+ * You can also define a module with connections like this:
+ *
+ * export const MyModule = (props: { connections: { GND: string, VCC: string } }) => {
+ *   return (
+ *     <group>
+ *       <capacitor name="C1" connections={{
+ *         anode: props.connections.GND,
+ *         cathode: props.connections.VCC,
+ *       }} />
+ *     </group>
+ *   )
+ * }
  */
 export type Connections<PinLabel extends string = string> = Partial<
   Record<
@@ -85,5 +103,34 @@ export type Connections<PinLabel extends string = string> = Partial<
  *     CUSTOM_DATA_2: ".U2 > .pin4",
  *   },
  * }
+ *
+ * A user can also use selectors to define the connections, this is helpful when
+ * there's multiple chips in the group.
+ *
+ * ```tsx
+ * const MyModule = (props: {
+ *   selectors: {
+ *     U1: { GND: string, VCC: string },
+ *     R1: { GND: string, VCC: string }
+ *   }
+ * }) => {
+ *   return (
+ *     <group>
+ *       <resistor name="R1" connections={{
+ *         pin1: props.selectors.R1.GND,
+ *         pin2: props.selectors.R1.VCC,
+ *       }} />
+ *       <capacitor name="C1" connections={{
+ *         anode: props.selectors.U1.GND,
+ *         cathode: props.selectors.U1.VCC,
+ *       }} />
+ *     </group>
+ *   )
+ * }
+ * ```
+ *
+ * These selectors can also be used with "sel":
+ *
+ * sel.M1(MyModule).U1.GND // ".M1 > .C1 > .anode"
  */
 export type Selectors = Record<string, Connections>

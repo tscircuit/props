9e298e38c2ad412fbc85eaff4573ec78bb6f8ae7
Abse <you@example.com>
2025-05-09 12:14:59 +0200

    added anchor and color to schematicTextProps for the <schematictext /> component

--- lib/components/schematic-text.ts
+++ lib/components/schematic-text.ts
@@ -1,3 +1,4 @@
+import { color } from "bun"
 import { distance } from "circuit-json"
 import { z } from "zod"
 
@@ -5,5 +6,7 @@ export const schematicTextProps = z.object({
   schX: distance,
   schY: distance,
   text: z.string(),
+  anchor: z.enum(["center", "left", "right", "top", "bottom"]).optional(),
+  color: z.string().optional(),
 })
 export type SchematicTextProps = z.input<typeof schematicTextProps>

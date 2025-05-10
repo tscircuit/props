3d50bccd55add69b0e24bca50ca192b7a87adcf9
Abse <you@example.com>
2025-05-10 13:43:38 +0200

    matched the props with cirucitJson type

--- lib/components/schematic-text.ts
+++ lib/components/schematic-text.ts
@@ -1,4 +1,4 @@
-import { distance } from "circuit-json"
+import { distance, rotation } from "circuit-json"
 import { z } from "zod"
 import { nine_point_anchor } from "lib/common/nine_point_anchor"
 import { five_point_anchor } from "lib/common/five_point_anchor"
@@ -8,7 +8,7 @@ export const schematicTextProps = z.object({
   schY: distance,
   text: z.string(),
   anchor: z.union([five_point_anchor.describe("legacy"), nine_point_anchor]),
-  color: z.string().optional(),
-  schRotation: z.number().optional(),
+  color: z.string().default("#000000"),
+  schRotation: rotation.default(0),
 })
 export type SchematicTextProps = z.input<typeof schematicTextProps>

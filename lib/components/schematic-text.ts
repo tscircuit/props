f32ee9280b2682787e0a4b1ef5b3fbee2f7d40dc
Abse <you@example.com>
2025-05-11 20:22:41 +0200

    Improved Pr:

--- lib/components/schematic-text.ts
+++ lib/components/schematic-text.ts
@@ -4,8 +4,8 @@ import { nine_point_anchor } from "lib/common/nine_point_anchor"
 import { five_point_anchor } from "lib/common/five_point_anchor"
 
 export const schematicTextProps = z.object({
-  schX: distance.default(0),
-  schY: distance.default(0),
+  schX: distance,
+  schY: distance,
   text: z.string(),
   fontSize: z.number().default(1),
   anchor: z

e7dd382c164bbe0a0d7c959ec341399879f645ca
Abse <you@example.com>
2025-05-11 19:18:47 +0200

    default values for anchor , schX and schY

--- lib/components/schematic-text.ts
+++ lib/components/schematic-text.ts
@@ -4,10 +4,12 @@ import { nine_point_anchor } from "lib/common/nine_point_anchor"
 import { five_point_anchor } from "lib/common/five_point_anchor"
 
 export const schematicTextProps = z.object({
-  schX: distance,
-  schY: distance,
+  schX: distance.default(0),
+  schY: distance.default(0),
   text: z.string(),
-  anchor: z.union([five_point_anchor.describe("legacy"), nine_point_anchor]),
+  anchor: z
+    .union([five_point_anchor.describe("legacy"), nine_point_anchor])
+    .default("center"),
   color: z.string().default("#000000"),
   schRotation: rotation.default(0),
 })

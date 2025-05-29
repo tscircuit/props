bed212bf710d225d11890afbb99eb272daef8cf5
Abse <you@example.com>
2025-05-29 19:17:34 +0200

    added padding directions

--- lib/components/schematic-box.ts
+++ lib/components/schematic-box.ts
@@ -11,5 +11,9 @@ export const schematicBoxProps = z.object({
   title: z.string().optional(),
   overlay: z.array(z.string()).optional(),
   strokeStyle: z.enum(["solid", "dashed"]).default("solid"),
+  paddingLeft: distance.default(0),
+  paddingRight: distance.default(0),
+  paddingTop: distance.default(0),
+  paddingBottom: distance.default(0),
 })
 export type SchematicBoxProps = z.input<typeof schematicBoxProps>

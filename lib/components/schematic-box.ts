01a2e5656aadde147af3bc301967494816e939e1
Abse <you@example.com>
2025-06-08 15:21:21 +0200

    patch

--- lib/components/schematic-box.ts
+++ lib/components/schematic-box.ts
@@ -1,4 +1,4 @@
-import { distance } from "circuit-json"
+import { distance, position } from "circuit-json"
 import { z } from "zod"
 import { nine_point_anchor } from "lib/common/nine_point_anchor"
 
@@ -17,12 +17,7 @@ export const schematicBoxProps = z
     paddingBottom: distance.optional(),
 
     title: z.string().optional(),
-    titleAnchorPosition: z
-      .object({
-        x: z.number().default(0),
-        y: z.number().default(0),
-      })
-      .optional(),
+    titleAnchorPosition: position.optional(),
     titleAnchorAlignment: nine_point_anchor.default("center"),
     titleColor: z.string().optional(),
     titleFontSize: distance.optional(),

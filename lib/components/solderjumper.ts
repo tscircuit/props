d72273bdf841e8f4b159174dd537834d7c4022d7
imrishabh18 <belikerishabh@gmail.com>
2025-06-24 23:58:05 +0530

    prop to bridge all the pins (used for bridged_ground symbol)

--- lib/components/solderjumper.ts
+++ lib/components/solderjumper.ts
@@ -7,10 +7,15 @@ export interface SolderJumperProps extends JumperProps {
    * Pins that are bridged with solder by default
    */
   bridgedPins?: string[][]
+  /**
+   * All pins are bridged with solder by default
+   */
+  bridged?: boolean
 }
 
 export const solderjumperProps = jumperProps.extend({
   bridgedPins: z.array(z.array(z.string())).optional(),
+  bridged: z.boolean().optional(),
 })
 
 type InferredSolderJumperProps = z.input<typeof solderjumperProps>

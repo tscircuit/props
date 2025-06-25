f3a90a81a52c4b7af374db14396690bef3e3dbc6
Ansh Grover <168731971+Anshgrover23@users.noreply.github.com>
2025-06-25 21:34:56 +0530

    add connections prop to led (#304)

--- lib/components/led.ts
+++ lib/components/led.ts
@@ -4,12 +4,17 @@ import {
   type SchematicOrientation,
 } from "lib/common/schematicOrientation"
 import { z } from "zod"
+import { createConnectionsProp } from "lib/common/connectionsProp"
+import type { Connections } from "lib/utility-types/connections-and-selectors"
+
+export type LedPinLabels = (typeof lrPolarPins)[number]
 
 export const ledProps = commonComponentProps.extend({
   color: z.string().optional(),
   wavelength: z.string().optional(),
   schDisplayValue: z.string().optional(),
   schOrientation: schematicOrientation.optional(),
+  connections: createConnectionsProp(lrPolarPins).optional(),
 })
 export const ledPins = lrPolarPins
 export type LedProps = z.input<typeof ledProps>

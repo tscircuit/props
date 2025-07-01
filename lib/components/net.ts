82a245fd8387366fb341742724e280117c953b87
Severin Ibarluzea <seve700@gmail.com>
2025-07-01 10:05:25 -0700

    feat(net): add connectsTo

--- lib/components/net.ts
+++ lib/components/net.ts
@@ -3,10 +3,12 @@ import { expectTypesMatch } from "lib/typecheck"
 
 export interface NetProps {
   name: string
+  connectsTo?: string | string[]
 }
 
 export const netProps = z.object({
   name: z.string(),
+  connectsTo: z.string().or(z.array(z.string())).optional(),
 })
 
 type InferredNetProps = z.input<typeof netProps>

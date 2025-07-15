8fe5b83482dbd67480e1bb190381e6901f8d6187
techmannih <manishchaudharyttt@gmail.com>
2025-07-15 09:33:03 +0530

    add connection props to transistor

--- lib/components/transistor.ts
+++ lib/components/transistor.ts
@@ -4,14 +4,31 @@ import {
 } from "lib/common/layout"
 import { expectTypesMatch } from "lib/typecheck"
 import { z } from "zod"
+import type { Connections } from "lib/utility-types/connections-and-selectors"
+import { createConnectionsProp } from "lib/common/connectionsProp"
+
+export const transistorPinsLabels = [
+  "pin1",
+  "pin2",
+  "pin3",
+  "emitter",
+  "collector",
+  "base",
+  "gate",
+  "source",
+  "drain",
+] as const
+export type transistorPinsLabels = (typeof transistorPinsLabels)[number]
 
 export interface TransistorProps<PinLabel extends string = string>
   extends CommonComponentProps<PinLabel> {
   type: "npn" | "pnp" | "bjt" | "jfet" | "mosfet" | "igbt"
+  connections?: Connections<transistorPinsLabels>
 }
 
 export const transistorProps = commonComponentProps.extend({
   type: z.enum(["npn", "pnp", "bjt", "jfet", "mosfet", "igbt"]),
+  connections: createConnectionsProp(transistorPinsLabels).optional(),
 })
 
 export const transistorPins = [

d615528549bfaeebe98bd9423678e948c1aa7ff1
Andrii Balitskyi <10balian10@gmail.com>
2025-07-31 19:09:49 +0200

    Add autocomplete for footprint prop

---
+++ tests/footprint-autocomplete.test.ts
@@ -0,0 +1,17 @@
+import { expect, test } from "bun:test"
+import { type FootprintProp, type BasicFootprint, type FootprintSoupElements } from "lib/common/footprintProp"
+import { AutocompleteString } from "lib/common/autocomplete"
+import { expectTypesMatch } from "lib/typecheck"
+
+// This test ensures the footprint prop accepts autocomplete strings
+
+expectTypesMatch<
+  FootprintProp,
+  | AutocompleteString<BasicFootprint>
+  | React.ReactElement
+  | FootprintSoupElements[]
+>(true)
+
+test("dummy", () => {
+  expect(true).toBe(true)
+})

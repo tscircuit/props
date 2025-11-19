12c11fe080441a5a4e82fdd767dcd69eb0547418
Ayush Jhawar <ayushjhawar499@gmail.com>
2025-11-19 23:17:36 +0530

    Feat: Introduce mustBeConnected prop

--- tests/pinAttributes.test.ts
+++ tests/pinAttributes.test.ts
@@ -18,3 +18,21 @@ test("pinAttributes allows doNotConnect", () => {
   expect(parsed.pinAttributes?.pin1?.includeInBoardPinout).toBe(false)
   expect(parsed.pinAttributes?.pin1?.highlightColor).toBe("#ff0000")
 })
+
+test("pinAttributes allows mustBeConnected", () => {
+  const rawProps = {
+    name: "chip",
+    pinAttributes: {
+      pin1: {
+        mustBeConnected: true,
+      },
+      pin2: {
+        mustBeConnected: false,
+      },
+    },
+  }
+
+  const parsed = chipProps.parse(rawProps)
+  expect(parsed.pinAttributes?.pin1?.mustBeConnected).toBe(true)
+  expect(parsed.pinAttributes?.pin2?.mustBeConnected).toBe(false)
+})

c476afda1fc13ee9ea62f5ed4f4858f4d8769d0b
imrishabh18 <belikerishabh@gmail.com>
2026-02-14 19:45:27 +0530

    Add the connections prop to the potentiometer

--- lib/components/potentiometer.ts
+++ lib/components/potentiometer.ts
@@ -1,22 +1,28 @@
 import { resistance } from "circuit-json"
+import { createConnectionsProp } from "lib/common/connectionsProp"
 import {
   type CommonComponentProps,
   commonComponentProps,
-  lrPins,
 } from "lib/common/layout"
 import { expectTypesMatch } from "lib/typecheck"
+import type { Connections } from "lib/utility-types/connections-and-selectors"
 import { z } from "zod"
 
 export type PotentiometerPinVariant = "two_pin" | "three_pin"
+export const potentiometerPinLabels = ["pin1", "pin2", "pin3"] as const
+export type PotentiometerPinLabels = (typeof potentiometerPinLabels)[number]
 
-export interface PotentiometerProps extends CommonComponentProps {
+export interface PotentiometerProps<PinLabel extends string = string>
+  extends CommonComponentProps<PinLabel> {
   maxResistance: number | string
   pinVariant?: PotentiometerPinVariant
+  connections?: Connections<PotentiometerPinLabels>
 }
 
 export const potentiometerProps = commonComponentProps.extend({
   maxResistance: resistance,
   pinVariant: z.enum(["two_pin", "three_pin"]).optional(),
+  connections: createConnectionsProp(potentiometerPinLabels).optional(),
 })
 
 type InferredPotentiometerProps = z.input<typeof potentiometerProps>

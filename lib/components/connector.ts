dda71b48f933df5847846ed33dcc508d7abe3cdc
Mustafa Mulla <mustafamulla765@gmail.com>
2026-04-01 22:10:28 +0530

    Extend ChipProps for ConnectorProps

--- lib/components/connector.ts
+++ lib/components/connector.ts
@@ -1,63 +1,15 @@
-import { distance } from "circuit-json"
-import {
-  type CommonComponentProps,
-  commonComponentProps,
-} from "lib/common/layout"
-import {
-  type SchematicPortArrangement,
-  schematicPortArrangement,
-} from "lib/common/schematicPinDefinitions"
-import {
-  type SchematicPinStyle,
-  schematicPinStyle,
-} from "lib/common/schematicPinStyle"
-import {
-  schematicPinLabel,
-  type SchematicPinLabel,
-} from "lib/common/schematicPinLabel"
+import { chipProps, type ChipPropsSU } from "./chip"
 import { expectTypesMatch } from "lib/typecheck"
 import { z } from "zod"
 
-export interface ConnectorProps extends CommonComponentProps {
-  manufacturerPartNumber?: string
-  pinLabels?: Record<
-    number | SchematicPinLabel,
-    SchematicPinLabel | SchematicPinLabel[]
-  >
-  schPinStyle?: SchematicPinStyle
-  schPinSpacing?: number | string
-  schWidth?: number | string
-  schHeight?: number | string
-  schDirection?: "left" | "right"
-  schPortArrangement?: SchematicPortArrangement
-  /**
-   * Groups of pins that are internally connected
-   * e.g., [["1","2"], ["2","3"]]
-   */
-  internallyConnectedPins?: (string | number)[][]
+export interface ConnectorProps extends ChipPropsSU {
   /**
    * Connector standard, e.g. usb_c, m2
    */
   standard?: "usb_c" | "m2"
 }
 
-export const connectorProps = commonComponentProps.extend({
-  manufacturerPartNumber: z.string().optional(),
-  pinLabels: z
-    .record(
-      z.number().or(schematicPinLabel),
-      schematicPinLabel.or(z.array(schematicPinLabel)),
-    )
-    .optional(),
-  schPinStyle: schematicPinStyle.optional(),
-  schPinSpacing: distance.optional(),
-  schWidth: distance.optional(),
-  schHeight: distance.optional(),
-  schDirection: z.enum(["left", "right"]).optional(),
-  schPortArrangement: schematicPortArrangement.optional(),
-  internallyConnectedPins: z
-    .array(z.array(z.union([z.string(), z.number()])))
-    .optional(),
+export const connectorProps = chipProps.extend({
   standard: z.enum(["usb_c", "m2"]).optional(),
 })
 

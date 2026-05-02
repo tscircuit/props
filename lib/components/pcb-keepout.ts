a8d5fd3184fe1d3201cef1d5e8dccb06a8e76040
Anas sarkiz <72264380+AnasSarkiz@users.noreply.github.com>
2026-05-03 00:36:45 +0200

    feat(keepout): allow both layer and layers props (#646)
    * feat(keepout): allow both layer and layers props
    
    * docs: Update README.md with latest component definitions [skip ci]
    
    ---------
    
    Co-authored-by: tscircuitbot <tscircuitbot@users.noreply.github.com>

--- lib/components/pcb-keepout.ts
+++ lib/components/pcb-keepout.ts
@@ -1,4 +1,4 @@
-import { distance } from "circuit-json"
+import { distance, layer_ref } from "circuit-json"
 import { pcbLayoutProps } from "lib/common/layout"
 import { z } from "zod"
 
@@ -6,11 +6,13 @@ export const pcbKeepoutProps = z.union([
   pcbLayoutProps.omit({ pcbRotation: true }).extend({
     shape: z.literal("circle"),
     radius: distance,
+    layers: z.array(layer_ref).optional(),
   }),
   pcbLayoutProps.extend({
     shape: z.literal("rect"),
     width: distance,
     height: distance,
+    layers: z.array(layer_ref).optional(),
   }),
 ])
 export type PcbKeepoutProps = z.input<typeof pcbKeepoutProps>

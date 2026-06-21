12f4cb7d41ec40464c871f3e4b6a1fb0dcedc0ef
Anil Kumar <anilkumar90875199@gmail.com>
2026-06-21 12:01:56 +0530

    Allow pcbRotation on courtyard rect props

--- lib/components/courtyard-rect.ts
+++ lib/components/courtyard-rect.ts
@@ -2,15 +2,13 @@ import { distance } from "circuit-json"
 import { pcbLayoutProps } from "lib/common/layout"
 import { z } from "zod"
 
-export const courtyardRectProps = pcbLayoutProps
-  .omit({ pcbRotation: true })
-  .extend({
-    width: distance,
-    height: distance,
-    strokeWidth: distance.optional(),
-    isFilled: z.boolean().optional(),
-    hasStroke: z.boolean().optional(),
-    isStrokeDashed: z.boolean().optional(),
-    color: z.string().optional(),
-  })
+export const courtyardRectProps = pcbLayoutProps.extend({
+  width: distance,
+  height: distance,
+  strokeWidth: distance.optional(),
+  isFilled: z.boolean().optional(),
+  hasStroke: z.boolean().optional(),
+  isStrokeDashed: z.boolean().optional(),
+  color: z.string().optional(),
+})
 export type CourtyardRectProps = z.input<typeof courtyardRectProps>

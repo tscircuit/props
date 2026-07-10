01cdfe0937219d2f86cbfa6f16e282119540bdb3
tscircuitbot <tscircuitbot@users.noreply.github.com>
2026-07-10 01:40:37 +0000

    formatbot: Automatically format code

--- lib/common/symbolProp.ts
+++ lib/common/symbolProp.ts
@@ -2,9 +2,6 @@ import type { AnyCircuitElement } from "circuit-json"
 import type { ReactElement } from "react"
 import { z } from "zod"
 
-export type SymbolProp =
-  | string
-  | ReactElement
-  | AnyCircuitElement[]
+export type SymbolProp = string | ReactElement | AnyCircuitElement[]
 
 export const symbolProp = z.custom<SymbolProp>((v) => true)
--- tests/symbol-prop.test.ts
+++ tests/symbol-prop.test.ts
@@ -3,12 +3,9 @@ import type { AnyCircuitElement } from "circuit-json"
 import type { SymbolProp } from "lib/common/symbolProp"
 import { expectTypesMatch } from "lib/typecheck"
 
-expectTypesMatch<
-  SymbolProp,
-  | string
-  | React.ReactElement
-  | AnyCircuitElement[]
->(true)
+expectTypesMatch<SymbolProp, string | React.ReactElement | AnyCircuitElement[]>(
+  true,
+)
 
 test("dummy", () => {
   expect(true).toBe(true)
